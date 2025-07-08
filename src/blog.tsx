4c4a5e0232a5a4ecffb8c5ff9a2ebb1cac624dce
Franco Gutierrez <cl_mate@hotmail.com>
2025-07-08 03:31:43 +0100

    updating style

--- src/blog.tsx
+++ src/blog.tsx
@@ -24,36 +24,34 @@ export default function Blog() {
     }
 
     return (
-        <div>
-            <section className="full-post">
-                <div className="container">
-                    <div className="row">
-                        <div className="blog">
-                            <div className="blog-post">
-                                <div className="thumbnail z-depth-1 animated">
-                                    <img src={fullImg} alt="" className="header-content" />
-                                    <div className="blog-details">
-                                        <div className="post-title">
-                                            <h3>{post.title}</h3>
-                                          
-                                        </div>
-                                        <div className="post-details">
-                                            <div className="author-name">
-                                                <h1>By: Franco Gutierrez</h1>
-                                                <h2>Date: {post.date}</h2>
-                                            </div>
-                                       
-                                            <div dangerouslySetInnerHTML={parsedContent}></div>
-                                         
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+      <div>
+        <section className="full-post">
+          <div className="container">
+            <div className="row">
+              <div className="blog">
+                <div className="blog-post">
+                  <div className="thumbnail z-depth-1 animated">
+                    <img src={fullImg} alt="" className="header-content" />
+                    <div className="blog-details">
+                      <div className="post-title">
+                        <h3>{post.title}</h3>
+                      </div>
+                      <div className="post-details">
+                        <div className="author-name">
+                          <h1>Franco Gutierrez</h1>
+                          <h2>Date: {post.date}</h2>
                         </div>
-                        <div className="clear"></div>
+
+                        <div dangerouslySetInnerHTML={parsedContent}></div>
+                      </div>
                     </div>
+                  </div>
                 </div>
-            </section>
-        </div>
+              </div>
+              <div className="clear"></div>
+            </div>
+          </div>
+        </section>
+      </div>
     );
 }
